bfb4254e8b6aa5aa21c94bf3ebf39d829332b5f5
Serena15 <raketa9390@mail.ru>
2016-01-19 00:39:01 +0300

    Update request_client_test.pl

--- net/t/request_client_test.pl
+++ net/t/request_client_test.pl
@@ -1,1 +1,17 @@
+use 5.010;
+use warnings;
+use strict;
+use Sfera::TCP::Calc::Client;
+use Sfera::TCP::Calc::Server;
 
+use IO::Socket::INET;
+my $socket = Sfera::TCP::Calc::Client::set_connect(1, 'localhost', 8081);
+
+while(1) {
+
+my $res = Sfera::TCP::Calc::Client::do_request(1, $socket, 1,  "2 + 2 * 2");
+#p $res;
+$res = Sfera::TCP::Calc::Client::do_request(1, $socket, 1,  "2 + 2 * 3");
+}
+
+close($socket);
